2676c956104b2915e046e45cc62dc677d50db1e8
jordanwhite1 <white.jordan92@gmail.com>
2013-04-20 14:15:33 -0400

    Adjusted Mod ID, and added a mod.info

--- aftercraft_common/core/lib/Reference.java
+++ aftercraft_common/core/lib/Reference.java
@@ -4,7 +4,7 @@ public class Reference {
 
 
     /* General Mod related constants */
-    public static final String MOD_ID = "AC";
+    public static final String MOD_ID = "AC_beta";
     public static final String MOD_NAME = "AfterCraft-Beta";
     public static final String VERSION = "0.01-Alpha";
     
